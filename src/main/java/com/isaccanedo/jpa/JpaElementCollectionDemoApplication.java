0f40df55609dc9836729daf9a63258a9d2f677c0
isaccanedo <isaccanedo@gmail.com>
2022-01-16 11:05:22 -0300

    Initial commit

---
+++ src/main/java/com/isaccanedo/jpa/JpaElementCollectionDemoApplication.java
@@ -0,0 +1,45 @@
+package com.isaccanedo.jpa;
+
+import com.isaccanedo.jpa.model.Address;
+import com.isaccanedo.jpa.model.User;
+import com.isaccanedo.jpa.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@SpringBootApplication
+public class JpaElementCollectionDemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaElementCollectionDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// Cleanup database tables.
+		userRepository.deleteAll();
+
+        // Insert a user with multiple phone numbers and addresses.
+        Set<String> phoneNumbers = new HashSet<>();
+		phoneNumbers.add("+91-9999999999");
+		phoneNumbers.add("+91-9898989898");
+
+		Set<Address> addresses = new HashSet<>();
+		addresses.add(new Address("747", "Golf View Road", "Bangalore",
+                "Karnataka", "India", "560008"));
+		addresses.add(new Address("Plot No 44", "Electronic City", "Bangalore",
+                "Karnataka", "India", "560001"));
+
+		User user = new User("Rajeev Kumar Singh", "rajeev@callicoder.com",
+                phoneNumbers, addresses);
+
+		userRepository.save(user);
+	}
+}
---
+++ src/test/java/com/isaccanedo/jpa/JpaElementCollectionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaccanedo.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JpaElementCollectionDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
